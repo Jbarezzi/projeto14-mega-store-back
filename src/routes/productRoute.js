3d1a904bba0685e25df4f7cc05f4603974d86afd
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-09 21:10:49 -0300

    feat: add get products route

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -3,5 +3,6 @@ import { Router } from "express";
 const router = Router();
 
 router.post("/create-product", validateNewProduct, createProduct);
+router.get("/products", getProducts);
 
 export default router;
